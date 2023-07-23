e8fe9527acd496367a5d647725e29e8c4c853b7f
yeonddori <lsymiso@naver.com>
2023-07-24 01:29:51 +0900

    feat: 로그인 정보 사이드바 추가

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { NavLink, Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
 
-function App() {
+const App = () => {
+  const [showNav, setShowNav] = useState(false);
+
+  const toggleNav = () => {
+    setShowNav((prevState) => !prevState);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app">
+        <header className="header">
+          <div className="navbar-container">
+            <nav className={`navbar ${showNav ? "active" : ""}`}>
+              <div className="nav-toggle" onClick={toggleNav}>
+                <div className={`bar ${showNav ? "bar-open" : ""}`} />
+                <div className={`bar ${showNav ? "bar-open" : ""}`} />
+                <div className={`bar ${showNav ? "bar-open" : ""}`} />
+              </div>
+              <ul className={`nav-items ${showNav ? "nav-items-show" : ""}`}>
+                <li>
+                  <NavLink exact to="/" activeClassName="active" onClick={toggleNav}>
+                    홈
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/about" activeClassName="active" onClick={toggleNav}>
+                    소개
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/contact" activeClassName="active" onClick={toggleNav}>
+                    문의
+                  </NavLink>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </header>
+        <main className="main">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </main>
+        <footer className="footer">
+          <p>© AID_WEB_FRONT</p>
+        </footer>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h2>안녕하세요! AIDeveloper 홈 페이지입니다.</h2>
+      <p>이곳은 설명을 적을 홈입니다.</p>
+    </div>
+  );
+};
+
+export default Home;
