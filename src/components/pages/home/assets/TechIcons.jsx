f41e9d519ba7ebc51f50a621755dd1c19b48febb
stopmin <geemin0@naver.com>
2023-08-14 02:23:15 +0900

    Auto stash before merge of "new_homepage" and "origin/new_homepage"

---
+++ src/components/pages/home/assets/TechIcons.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const TechIcons = () => {
+  return (
+    <div>
+      <div style={{ display: "flex", alignItems: "flex-start" }}>
+        <img src="https://techstack-generator.vercel.app/python-icon.svg" alt="icon" width="65" height="65" />
+      </div>
+      <div style={{ display: "flex", alignItems: "flex-start" }}>
+        <img src="https://techstack-generator.vercel.app/github-icon.svg" alt="icon" width="65" height="65" />
+      </div>
+      <div style={{ display: "flex", alignItems: "flex-start" }}>
+        <img src="https://techstack-generator.vercel.app/react-icon.svg" alt="icon" width="65" height="65" />
+      </div>
+      <div style={{ display: "flex", alignItems: "flex-start" }}>
+        <img src="https://techstack-generator.vercel.app/prettier-icon.svg" alt="icon" width="65" height="65" />
+      </div>
+    </div>
+  );
+};
+
+export default TechIcons;
