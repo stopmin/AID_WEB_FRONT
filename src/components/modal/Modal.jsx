4576df99bb952b0b5bb47a7102bf02a54fe4eb4a
yeonddori <lsymiso@naver.com>
2023-08-18 20:06:01 +0900

    feat: axios: GET, PUT, DELETE

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,19 +1,39 @@
-import { React, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Modal from "react-modal";
 import axios from "axios";
+import { FormField, FormFieldTextArea } from "../pages/submit/Submit";
+
+Modal.setAppElement("#root");
 
 const ModalExample = () => {
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const [text, setText] = useState("");
   const [submit_id, setSubmitId] = useState("");
+  const [applicantData, setApplicantData] = useState(null);
 
   useEffect(() => {
     console.log(submit_id);
-    // 여기에 submit_id가 변경될 때 수행하고 싶은 작업을 추가할 수 있습니다.
+
+    if (submit_id) {
+      axios
+        .get(`https://pnuaid.com/api/submit/read?submit_id=${submit_id}`)
+        .then((response) => {
+          setApplicantData(response.data);
+          console.log(response.data);
+        })
+        .catch((error) => {
+          console.error("GET error:", error);
+          setApplicantData(null);
+        });
+    } else {
+      setApplicantData(null);
+    }
   }, [submit_id]);
 
   const openModal = () => {
     setModalIsOpen(true);
+    setSubmitId("");
+    setApplicantData(null);
   };
 
   const closeModal = () => {
@@ -24,9 +44,34 @@ const ModalExample = () => {
     setText(e.target.value);
   };
 
-  const onClick = () => {
+  const onClick = async () => {
     setSubmitId(text);
-    console.log(submit_id); // 이 시점에서는 업데이트되기 전의 값이 출력됩니다.
+  };
+
+  const updateApplicantData = async () => {
+    try {
+      await axios.put(`https://pnuaid.com/api/submit/modify?submit_id=${submit_id}`, applicantData);
+      console.log("PUT success");
+      const result = axios.get(`https://pnuaid.com/api/submit/read?submit_id=${submit_id}`);
+      console.log(result);
+      closeModal();
+    } catch (error) {
+      console.error("PUT error:", error);
+    }
+  };
+
+  const deleteApplicantData = async () => {
+    try {
+      await axios.delete(`https://pnuaid.com/api/submit/cancle?submit_id=${submit_id}`);
+      console.log("DELETE success");
+      closeModal();
+      setSubmitId("");
+      setApplicantData(null);
+      // const result = axios.get(`https://pnuaid.com/api/submit/read?submit_id=${submit_id}`);
+      // console.log(result);
+    } catch (error) {
+      console.error("DELETE error:", error);
+    }
   };
 
   return (
@@ -38,11 +83,74 @@ const ModalExample = () => {
         <div>
           <input type="text" id="applicant_id" placeholder="ID를 입력해주세요" onChange={onChange}></input>
           <button onClick={onClick}>조회하기</button>
-          <p>입력값: {submit_id}</p>
+          {/* <p>입력값: {submit_id}</p> */}
         </div>
 
-        <h3>내 지원서</h3>
-        <button onClick={closeModal}>재등록</button>
+        {!applicantData ? (
+          <p>지원서 내용을 조회해주세요.</p>
+        ) : (
+          <>
+            <h3>내 지원서</h3>
+            <form>
+              <FormField
+                label="이름"
+                id="name"
+                value={applicantData.name}
+                onChange={(e) => setApplicantData({ ...applicantData, name: e.target.value })}
+                placeholder="홍길동"
+                required
+              />
+              <FormField
+                label="이메일"
+                id="email"
+                value={applicantData.email}
+                onChange={(e) => setApplicantData({ ...applicantData, email: e.target.value })}
+                placeholder="test@naver.com"
+                required
+              />
+              <FormField
+                label="학번(9자리)"
+                id="student_id"
+                value={applicantData.student_id}
+                onChange={(e) => setApplicantData({ ...applicantData, student_id: e.target.value })}
+                placeholder="202012345"
+                required
+              />
+              <FormFieldTextArea
+                label="동아리 지원 계기(공백 포함 500자 이내)"
+                id="motivation"
+                value={applicantData.motivation}
+                onChange={(e) => setApplicantData({ ...applicantData, motivation: e.target.value })}
+                rows="3"
+                name="motivation"
+              />
+              <FormFieldTextArea
+                label="프로젝트 경험"
+                id="ai_exp"
+                value={applicantData.ai_exp}
+                onChange={(e) => setApplicantData({ ...applicantData, ai_exp: e.target.value })}
+                rows="3"
+                name="have_project"
+              />
+              <FormField
+                label="Github 주소"
+                id="github"
+                value={applicantData.github}
+                onChange={(e) => setApplicantData({ ...applicantData, github: e.target.value })}
+              />
+              <FormField
+                label="Blog 주소(Tech Blog)"
+                id="blog"
+                value={applicantData.blog}
+                onChange={(e) => setApplicantData({ ...applicantData, blog: e.target.value })}
+              />
+            </form>
+            <div style={{ display: "flex", justifyContent: "space-between", marginTop: "10px" }}>
+              <button onClick={updateApplicantData}>재등록</button>
+              <button onClick={deleteApplicantData}>지원서 삭제</button>
+            </div>
+          </>
+        )}
       </Modal>
     </div>
   );
--- src/components/pages/submit/Submit.jsx
+++ src/components/pages/submit/Submit.jsx
@@ -1,8 +1,7 @@
 import { React, useState } from "react";
-import "./assets/submit.css";
-import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import { Modal } from "../../modal/Modal";
+import axios from "axios";
+import "./assets/submit.css";
 
 const FormField = ({ label, id, value, onChange, ...props }) => (
   <div className="form-group">
