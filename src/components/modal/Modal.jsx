4f5ce6e29268642cc0ab006b9ac23a1e0c85e8cc
stopmin <geemin0@naver.com>
2023-08-18 13:20:49 +0900

    fix: modify useState -> useEffect

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,32 +1,32 @@
-import { React, useState } from "react";
+import { React, useState, useEffect } from "react";
 import Modal from "react-modal";
 import axios from "axios";
 
 const ModalExample = () => {
   const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [text, setText] = useState("");
+  const [submit_id, setSubmitId] = useState("");
 
-  // 팝업창 open
+  useEffect(() => {
+    console.log(submit_id);
+    // 여기에 submit_id가 변경될 때 수행하고 싶은 작업을 추가할 수 있습니다.
+  }, [submit_id]);
+
   const openModal = () => {
     setModalIsOpen(true);
   };
 
-  // 팝업창 close
   const closeModal = () => {
     setModalIsOpen(false);
   };
 
-  const [text, setText] = useState("");
-  const [submit_id, setName] = useState("");
-
-  // 입력한 값 text로 저장
   const onChange = (e) => {
     setText(e.target.value);
   };
 
-  // 입력한 id로 axios를 통해 이전에 작성한 내역 get
   const onClick = () => {
-    setName(text);
-    console.log(submit_id);
+    setSubmitId(text);
+    console.log(submit_id); // 이 시점에서는 업데이트되기 전의 값이 출력됩니다.
   };
 
   return (
@@ -38,7 +38,7 @@ const ModalExample = () => {
         <div>
           <input type="text" id="applicant_id" placeholder="ID를 입력해주세요" onChange={onChange}></input>
           <button onClick={onClick}>조회하기</button>
-          <p>입력값: {submit_id}</p> {/* id 있다고 가정했을 때 */}
+          <p>입력값: {submit_id}</p>
         </div>
 
         <h3>내 지원서</h3>
