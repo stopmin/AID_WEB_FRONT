756c5b11300677793a878797fa8fea79f76d18fd
yeonddori <lsymiso@naver.com>
2023-08-21 13:27:05 +0900

    feat: add alert & usState

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,19 +1,49 @@
-import { React, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Modal from "react-modal";
 import axios from "axios";
+import { FormField, FormFieldTextArea } from "../pages/submit/Submit";
+import "./assets/modal.css";
+
+Modal.setAppElement("#root");
 
 const ModalExample = () => {
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const [text, setText] = useState("");
   const [submit_id, setSubmitId] = useState("");
+  const [applicantData, setApplicantData] = useState(null);
+  const [isDataFetched, setIsDataFetched] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
   useEffect(() => {
     console.log(submit_id);
-    // 여기에 submit_id가 변경될 때 수행하고 싶은 작업을 추가할 수 있습니다.
+
+    if (submit_id) {
+      axios
+        .get(`https://pnuaid.com/api/submit/read?submit_id=${submit_id}`)
+        .then((response) => {
+          setApplicantData(response.data);
+          setIsDataFetched(true);
+          console.log(response.data);
+        })
+        .catch((error) => {
+          console.error("GET error:", error);
+          setApplicantData(null);
+          setIsDataFetched(true);
+          setErrorMessage("ID를 다시 확인해주세요.");
+        });
+    } else {
+      setApplicantData(null);
+      setIsDataFetched(false);
+      setErrorMessage("");
+    }
   }, [submit_id]);
 
   const openModal = () => {
     setModalIsOpen(true);
+    setSubmitId("");
+    setApplicantData(null);
+    setIsDataFetched(false);
+    setErrorMessage("");
   };
 
   const closeModal = () => {
@@ -24,25 +54,127 @@ const ModalExample = () => {
     setText(e.target.value);
   };
 
-  const onClick = () => {
+  const onClick = async () => {
     setSubmitId(text);
-    console.log(submit_id); // 이 시점에서는 업데이트되기 전의 값이 출력됩니다.
+    setIsDataFetched(false);
+  };
+
+  const updateApplicantData = async () => {
+    try {
+      await axios.put(`https://pnuaid.com/api/submit/modify?submit_id=${submit_id}`, applicantData);
+      console.log("PUT success");
+      const result = axios.get(`https://pnuaid.com/api/submit/read?submit_id=${submit_id}`);
+      console.log(result);
+      closeModal();
+      alert("수정이 완료되었습니다.");
+    } catch (error) {
+      console.error("PUT error:", error);
+    }
+  };
+
+  const deleteApplicantData = async () => {
+    try {
+      await axios.delete(`https://pnuaid.com/api/submit/cancle?submit_id=${submit_id}`);
+      console.log("DELETE success");
+      closeModal();
+      setSubmitId("");
+      setApplicantData(null);
+      setIsDataFetched(false);
+      setErrorMessage("");
+      alert("삭제가 완료되었습니다.");
+      // const result = axios.get(`https://pnuaid.com/api/submit/read?submit_id=${submit_id}`);
+      // console.log(result);
+    } catch (error) {
+      console.error("DELETE error:", error);
+    }
   };
 
   return (
     <div>
-      <button onClick={openModal}>내 지원서 수정하기</button>
-      <Modal isOpen={modalIsOpen} onRequestClose={closeModal}>
-        <h2>지원서 수정</h2>
-        <div>내 지원서 조회: </div>
-        <div>
-          <input type="text" id="applicant_id" placeholder="ID를 입력해주세요" onChange={onChange}></input>
-          <button onClick={onClick}>조회하기</button>
-          <p>입력값: {submit_id}</p>
-        </div>
+      <button className="no-underline" onClick={openModal}>
+        내 지원서 수정하기
+      </button>
+      <Modal isOpen={modalIsOpen} onRequestClose={closeModal} overlayClassName="overlay-modal">
+        <div className="content-modal">
+          <h2>지원서 수정</h2>
+          <div className="submit-check">
+            {/* 내 지원서 조회: */}
+            <input type="text" id="applicant_id" placeholder="ID를 입력해주세요" onChange={onChange}></input>
+            <button onClick={onClick} className="button-modal">
+              조회하기
+            </button>
+          </div>
 
-        <h3>내 지원서</h3>
-        <button onClick={closeModal}>재등록</button>
+          {errorMessage && !applicantData && <p>{errorMessage}</p>}
+          {!errorMessage && !isDataFetched && <p>지원서 내용을 조회해주세요.</p>}
+          {applicantData && (
+            <>
+              {/* <h3>내 지원서</h3> */}
+              <form>
+                <FormField
+                  label="이름"
+                  id="name"
+                  value={applicantData.name}
+                  onChange={(e) => setApplicantData({ ...applicantData, name: e.target.value })}
+                  placeholder="홍길동"
+                  required
+                />
+                <FormField
+                  label="이메일"
+                  id="email"
+                  value={applicantData.email}
+                  onChange={(e) => setApplicantData({ ...applicantData, email: e.target.value })}
+                  placeholder="test@naver.com"
+                  required
+                />
+                <FormField
+                  label="학번(9자리)"
+                  id="student_id"
+                  value={applicantData.student_id}
+                  onChange={(e) => setApplicantData({ ...applicantData, student_id: e.target.value })}
+                  placeholder="202012345"
+                  required
+                />
+                <FormFieldTextArea
+                  label="동아리 지원 계기(공백 포함 500자 이내)"
+                  id="motivation"
+                  value={applicantData.motivation}
+                  onChange={(e) => setApplicantData({ ...applicantData, motivation: e.target.value })}
+                  rows="3"
+                  name="motivation"
+                />
+                <FormFieldTextArea
+                  label="프로젝트 경험"
+                  id="ai_exp"
+                  value={applicantData.ai_exp}
+                  onChange={(e) => setApplicantData({ ...applicantData, ai_exp: e.target.value })}
+                  rows="3"
+                  name="have_project"
+                />
+                <FormField
+                  label="Github 주소"
+                  id="github"
+                  value={applicantData.github}
+                  onChange={(e) => setApplicantData({ ...applicantData, github: e.target.value })}
+                />
+                <FormField
+                  label="Blog 주소(Tech Blog)"
+                  id="blog"
+                  value={applicantData.blog}
+                  onChange={(e) => setApplicantData({ ...applicantData, blog: e.target.value })}
+                />
+              </form>
+              <div className="button-flex">
+                <button className="button-modal" onClick={deleteApplicantData}>
+                  지원서 삭제
+                </button>
+                <button className="button-modal" onClick={updateApplicantData}>
+                  수정
+                </button>
+              </div>
+            </>
+          )}
+        </div>
       </Modal>
     </div>
   );
